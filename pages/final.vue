860012aecb31383bb84eebb8356b82551d223d05
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-17 10:34:55 +0700

    Master: comment final - global css

--- pages/final.vue
+++ pages/final.vue
@@ -1,21 +1,21 @@
-<template>
-  <div class="container">
-    <p>Final Page</p>
-    <nuxt-link to="/">
-      Go to /
-    </nuxt-link>
-  </div>
-</template>
+<!--<template>-->
+<!--  <div class="container">-->
+<!--    <p>Final Page</p>-->
+<!--    <nuxt-link to="/">-->
+<!--      Go to /-->
+<!--    </nuxt-link>-->
+<!--  </div>-->
+<!--</template>-->
 
-<script>
-export default {
-}
-</script>
+<!--<script>-->
+<!--export default {-->
+<!--}-->
+<!--</script>-->
 
-<style scoped>
-  .container {
-    font-size: 20px;
-    text-align: center;
-    padding-top: 100px;
-  }
-</style>
+<!--<style scoped>-->
+<!--  .container {-->
+<!--    font-size: 20px;-->
+<!--    text-align: center;-->
+<!--    padding-top: 100px;-->
+<!--  }-->
+<!--</style>-->
